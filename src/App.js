8b561613fd3fd874854cd3e764fbce7724d44ec2
Your Name <you@example.com>
2024-02-22 21:36:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes,Route} from "react-router-dom"
 import './App.css';
+import Modal from "./component/Modal";
+import Edit from "./component/Edit";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Modal/>}/>
+        <Route path="/edit" element={<Edit/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
